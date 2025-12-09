d0915178e4520c53fc0e72381bba3c67596a6451
LuisRocha <149401326+Luiss2080@users.noreply.github.com>
2025-12-09 15:05:56 -0400

    feat: add `web-app.blade.php` base layout for the web application.

--- resources/views/layouts/web-app.blade.php
+++ resources/views/layouts/web-app.blade.php
@@ -8,6 +8,9 @@
     
     <!-- Favicon -->
     <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
+
+    <!-- FontAwesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     
     
     <!-- CSS Componentes -->
